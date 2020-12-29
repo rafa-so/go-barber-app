da27283294c3e531b81c990a4ff4321fc34a2c3e
Rafael Oliveira <raaf.forsoft.25@gmail.com>
2020-12-28 23:07:44 -0300

    init build sign in page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
@@ -8,6 +12,11 @@ const SignIn: React.FC = () => {
   return <Container>
     <Image source={logoImg} />
     <Title>Faça seu logon</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button onPress={() => { }}>Entrar</Button>
   </Container>
 }
 
